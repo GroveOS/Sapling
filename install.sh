d8e9c0969ef5cce4ae4b98b2003a392ee4d68cc5
Matthew <info@goanatomic.com>
2022-03-12 22:21:33 -0500

    refines install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-cp -r sapling/embed ./
-cp -r sapling/sc ./
+cp -r Sapling/embed ./
+cp -r Sapling/sc ./
 for item in head header footer; do
 	if [[ ! -f embed/sapling/layout/$item.html ]]; then
-		mv embed/sapling/layout/$item.example.html embed/sampling/layout/$item.html
+		mv embed/sapling/layout/$item.example.html embed/sapling/layout/$item.html
 	fi
 done
+rm -rf Sapling
