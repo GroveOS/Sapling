12b2bf0d9b46c63dacf64b1cf8713cb1886a8a54
Matthew <info@goanatomic.com>
2022-03-12 22:50:32 -0500

    improves install.sh script to suggest updated files

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@ cp -r Sapling/sc ./
 for item in head header footer; do
 	if [[ ! -f embed/sapling/layout/$item.html ]]; then
 		mv embed/sapling/layout/$item.example.html embed/sapling/layout/$item.html
+	else
+		mv embed/sapling/layout/$item.example.html embed/sapling/layout/$item.updated.html
 	fi
 done
+if [[ ! -f sc/config.sh ]]; then
+	mv sc/config.example.sh sc/config.sh
+else
+	mv sc/config.example.sh sc/config.updated.sh
+fi
 rm -rf Sapling
