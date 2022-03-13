31c9ead6a697a96a77ee8129be17ba0e8c684bf8
Matthew <info@goanatomic.com>
2022-03-13 19:18:01 -0400

    updates the update.sh script

--- sc/update.sh
+++ sc/update.sh
@@ -1,9 +1,11 @@
+. sc/core.sh
+
 if [[ $1 == 'couch' ]]; then
 	git clone https://github.com/CouchCMS/CouchCMS
-	cp -r CouchCMS/couch/* couch/
+	cp -r CouchCMS/couch/* $couch_dir/
 	rm -rf CouchCMS
 elif [[ $1 == 'sapling' ]]; then
 	git clone https://github.com/GroveOS/Sapling
-	Sapling/install.sh
+	cp -r Sapling/sapling/* $snippets_dir/sapling/
 	rm -rf Sapling
 fi
