ec610f8179c3a3b1baf4528807d0235af9c7828d
Matthew <info@goanatomic.com>
2022-03-19 17:39:55 -0400

    refines install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,23 @@
 # Prepare installation based on arguments
 if [[ $1 == 'from-scratch' ]]; then
+	# Clone CouchCMS
 	git clone https://github.com/CouchCMS/CouchCMS
 	mv CouchCMS/couch ./
 	rm -rf CouchCMS
-	couch_dir="couch"
+	# Set up default snippets directory
 	mkdir embed
+	# Establish default variables
+	couch_dir="couch"
 	snippets_dir="embed"
 	sapling_commands_dir="sc"
 elif [[ $1 == 'alongside-couch' ]]; then
 	if [[ $3 == 'default' ]]; then
+		# Establish default variables alongside existing couch
 		couch_dir=$2
 		snippets_dir="embed"
 		sapling_commands_dir="sc"
 	else
+		# Establish custom variables
 		couch_dir=$2
 		snippets_dir=$3
 		sapling_commands_dir=$4
@@ -23,52 +28,62 @@ else
 	sapling_commands_dir=$3
 fi
 
-if [[ ! $couch_dir ]]; then
-	echo "No Couch directory provided. Aborted."
+
+
+
+##### CHECK FOR VARIABLE ERRORS ######
+# Check that arguments were given
+if [[ ! $couch_dir || ! $snippets_dir || ! $sapling_commands_dir ]]; then
+	echo "Missing arguments, try again."
 	exit 0
 fi
-
-if [[ ! $snippets_dir ]]; then
-	echo "No Couch snippets directory provided. Aborted."
-	exit 0
-fi
-
-if [[ ! $sapling_commands_dir ]]; then
-	echo "No Sapling commands directory provided. Aborted."
-	exit 0
-fi
-
-# Set up sapling directory and config.sh file
-mkdir $sapling_commands_dir
-echo "couch_dir=\"$couch_dir\"" >> $sapling_commands_dir/config.sh
-echo "snippets_dir=\"$snippets_dir\"" >> $sapling_commands_dir/config.sh
-echo "sapling_commands_dir=\"$sapling_commands_dir\"" >> $sapling_commands_dir/config.sh
-
-if [[ -d $couch_dir && -d $snippets_dir && $sapling_commands_dir ]]; then
+# Check that Couch and Snippets directories exists
+if [[ -d $couch_dir && -d $snippets_dir ]]; then
 	echo "Sapling installing . . ."
 else
 	echo "Something went wrong with your directory setup."
 	exit 0
 fi
 
-cp -r Sapling/embed ./
-cp -r Sapling/sc ./
+
+
+
+# Set up sapling directory and prepare config.sh file
+mkdir $sapling_commands_dir
+echo "couch_dir=\"$couch_dir\"\nsnippets_dir=\"$snippets_dir\"\nsapling_commands_dir=\"$sapling_commands_dir\"" >> $sapling_commands_dir/config.sh
+
+
+
+
+
+
+##### INSTALLTION #####
+
+cp -r Sapling/embed ./$snippets_dir
+cp -r Sapling/sc ./$sapling_commands_dir
+
 if [[ $1 == 'from-scratch' ]]; then
-	# Copy Sapling flavored config.php to couch/config.php
-	cp Sapling/embed/sapling/lib/sapling.config.php couch/config.php
+	# Install default Sapling flavored Couch configs 
+	cp -r $snippets_dir/sapling/lib/couch/* $couch_dir/
 fi
 
+# sapling/config.json
 if [[ ! -f $snippets_dir/sapling/config.json ]]; then
-	mv embed/sapling/config.example.json embed/sapling/config.json
+	mv $snippets_dir/sapling/config.example.json $snippets_dir/sapling/config.json
 fi
 
+# sapling/addons/kfunctions.php
+if [[ ! -f $snippets_dir/sapling/addons/kfunctions.php ]]; then
+	mv $snippets_dir/sapling/addons/kfunctions.example.php $snippets_dir/sapling/addons/kfunctions.php
+fi
+
+# sapling/layout/ head header footer
 for item in head header footer; do
 	if [[ ! -f $snippets_dir/sapling/layout/$item.html ]]; then
 		mv $snippets_dir/sapling/layout/sapling.$item.html $snippets_dir/sapling/layout/$item.html
-	else
-		mv $snippets_dir/sapling/layout/sapling.$item.html $snippets_dir/sapling/layout/$item.updated.html
 	fi
 done
-rm -rf Sapling
 
+# Clean up
+rm -rf Sapling
 echo "Sapling installed."
